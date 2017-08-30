64faf87bd7a1e61dac7dab6ac8e0acbe7021739f
SergeyMiracle <sergeymiracle@gmail.com>
2017-08-30 22:03:33 +0800

    spelling

--- config/uploadable.php
+++ config/uploadable.php
@@ -5,6 +5,6 @@ return [
 
   'images' => [
     'optimize' => false,  // optimize images using spatie/image-optimizer
-    'max_heigth' => 900  // auto resize image to a height when optimizing and constrain aspect ratio (auto width)
+    'max_height' => 900  // auto resize image to a height when optimizing and constrain aspect ratio (auto width)
   ]
 ];
